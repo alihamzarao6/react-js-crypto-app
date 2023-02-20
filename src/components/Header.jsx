2de7339eae3ce294492b379f80c861cb0ddf9ef4
Ali Hamza <alihamzarao6>
2023-02-21 04:36:13 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HStack, Button } from '@chakra-ui/react';
+
+const Header = () => {
+    return (
+        <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
+            <Button variant={"unstyled"} color={"white"} p={'2'}>
+                <Link to="/">Home</Link>
+            </Button>
+
+            <Button variant={"unstyled"} color={"white"} p={'2'}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Button>
+            
+            <Button variant={"unstyled"} color={"white"} p={'2'}>
+                <Link to="/coins">Coins</Link>
+            </Button>
+        </HStack>
+    )
+}
+
+export default Header;
